0b6b715002a81fedb01d750c89636b295f2aa20b
jasmine <jasliu2012@gmail.com>
2018-01-23 21:33:17 -0600

    refined stop words array to only show a list of words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -20,5 +20,23 @@ class WordsController < ApplicationController
       p unrefined_stop_words_array = f.drop(12)
     end
     p 'X' * 50
+
+    
+    ###################### BEGIN STOP_WORDS FILTERING ########################
+
+    # refine the stop words array so that we only have a list of words.
+    refined_stop_words = []
+
+    unrefined_stop_words_array.each do |stop_word|
+      chomped_word = stop_word.chomp
+      if !chomped_word.empty?
+        refined_stop_words << chomped_word
+      end
+    end
+
+    ###################### END STOP_WORDS FILTERING ##########################
+
+
+
   end
 end
