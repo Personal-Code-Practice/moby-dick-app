317253c899fe44d8086d0484396f84c506a31e88
jasmine <jasliu2012@gmail.com>
2018-01-23 21:21:23 -0600

    Found pathname to txt files located in public folder

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,13 @@
 class WordsController < ApplicationController
   def index
     @words = "Herman Melville's Moby Dick"
+
+    ### OBJECTIVE: create a list of the top 100 most frequently occurring words (excluding stop words) paired with the count occurrence of the word in the text of Moby Dick.
+
+    # import files. Drop the lines that are not part of the actual text.
+    p "*" * 50
+    p path_text = Rails.root.join('public', 'mobydick.txt').class
+    p path_stop_words = Rails.root.join('public', 'stop-words.txt').class
+    p "*" * 50
   end
 end
